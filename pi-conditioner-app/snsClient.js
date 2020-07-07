af726ecb586497beee4bb4e82f4a0f7ee7fc1774
jmflatt <jonathanm.flatt@gmail.com>
2020-07-06 23:25:35 -0400

    attempt to send an sms message to my self

--- pi-conditioner-app/snsClient.js
+++ pi-conditioner-app/snsClient.js
@@ -8,8 +8,7 @@ module.exports = {
     sendSnsMessage: async function(temp, isOn) {
         const params = {
             Message: `Status: ${isOn}. Current Temperature: ${temp.temperature}. Current Humidity: ${temp.humidity}`, /* required */
-            PhoneNumber: '+15138335254',
-            Topic
+            PhoneNumber: '+15138335254'
           };
           var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
 
