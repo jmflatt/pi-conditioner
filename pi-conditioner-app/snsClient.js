035bc8ec6817fb5c533d4690537cd4f48d35ed52
jmflatt <jonathanm.flatt@gmail.com>
2020-07-06 23:18:58 -0400

    attempt to send an sms message to my self

--- pi-conditioner-app/snsClient.js
+++ pi-conditioner-app/snsClient.js
@@ -1,7 +1,7 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set region
-AWS.config.update({region: 'us-east-2'});
+AWS.config.update({region: 'us-east-1'});
 
 module.exports = {
     
